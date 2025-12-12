8e9cd332f42b27a81f9014edae8db83817a0175f
אמיר אבו <amir.abu300@gmail.com>
2025-12-12 16:28:12 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -2,6 +2,7 @@ import Home from './pages/Home';
 import Admin from './pages/Admin';
 import Terms from './pages/Terms';
 import DisplayTest from './pages/DisplayTest';
+import Display from './pages/Display';
 
 
 export const PAGES = {
@@ -9,6 +10,7 @@ export const PAGES = {
     "Admin": Admin,
     "Terms": Terms,
     "DisplayTest": DisplayTest,
+    "Display": Display,
 }
 
 export const pagesConfig = {
